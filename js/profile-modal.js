9a22cea29744d98026a4a51d7b9f601e4bbeef35
LeeGuene <tlsdlrms1@naver.com>
2024-07-04 17:51:30 +0900

    페이지별 모달 추가

---
+++ js/profile-modal.js
@@ -0,0 +1,18 @@
+// 프로필 이미지 변경 모달창
+const profile_modal = document.querySelector('.modal-profile');
+const profile_btnOpenModal=document.querySelector('.btn-profile-modal');
+const profile_btnCloseModal=document.querySelector('.close-profile-btn');
+const profile_btnCloseModal2=document.querySelector('#close-profile-btn');
+
+
+profile_btnOpenModal.addEventListener("click", ()=>{
+    profile_modal.style.display="flex";
+});
+
+profile_btnCloseModal.addEventListener("click", ()=>{
+    profile_modal.style.display="none";
+});
+
+profile_btnCloseModal2.addEventListener("click", ()=>{
+    profile_modal.style.display="none";
+});
